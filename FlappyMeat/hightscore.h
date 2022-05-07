e3b6c64aa32b9ea62712b334e6eec80acafa9769
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2022-05-07 23:56:00 +0300

    Beta is here!

---
+++ FlappyMeat/hightscore.h
@@ -0,0 +1,14 @@
+#ifndef HIGHTSCORE_H
+#define HIGHTSCORE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class hightscore
+{
+public:
+void GetHightscore();
+
+};
+
+#endif // HIGHTSCORE_H
