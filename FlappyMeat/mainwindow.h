2b810784abe884c276cbd7900c1972f5e4bdc02b
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2022-05-28 22:47:03 +0300

    FlappyMeat V1.0a
    -Added oportunity to use your own sprites
    -Added icon
    -Reworked Settings menu
    -Sorry Zhenya without slider(because even with speed 2 it's quite unplayble)

---
+++ FlappyMeat/mainwindow.h
@@ -0,0 +1,147 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QLabel>
+#include <QMainWindow>
+#include <QPixmap>
+#include <vector>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QTimer>
+#include <QKeyEvent>
+#include <QWidget>
+#include <QTextDocument>
+#include <QtMultimedia>
+#include <QSoundEffect>
+#include <QFile>
+#include <QTextStream>
+#include <QTransform>
+#include <QPushButton>
+#include <QApplication>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QIcon>
+
+#include "pipe.h"
+#include "meatpiece.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    Ui::MainWindow *ui;
+
+
+public slots:
+    void Update();
+
+    void Init();
+
+    void paintEvent(QPaintEvent* painevent) ;
+
+    void Inrtersects();
+
+    void Score();
+
+    void Restart();
+
+    void ButtonChecker();
+
+    void randomanimation();
+
+
+
+private slots:
+
+   void keyPressEvent(QKeyEvent *event) ;
+
+   void StartGame();
+
+   void EndGame();
+
+   void Replay();
+
+   void GoToMenu();
+
+   void Settings();
+
+   void Skins();
+
+   void Easy();
+   void Medium();
+   void Hard();
+
+   void DifficultyChecker();
+
+   void bird1();
+   void bird2();
+   void bird3();
+   void birdcustom();
+   void choosebird();
+
+   void background_origin();
+   void background_custom();
+   void background_custom_picker();
+
+private:
+
+    double result = 0;
+
+    static const int kPipesCount;
+    static const int kDistanceBetweenPipes;
+    static const int kMinPipeHeight;
+    static int kTopBottomSpacing;
+
+
+    QTimer* timer ;
+    std::vector<Pipe> pipes;
+    QPixmap* pipe_pixmap_up;
+    QPixmap* pipe_pixmap_d;
+    QPixmap* bird;
+    QPixmap* presset;
+    QPixmap* Scoreboard;
+    QPixmap* smoke;
+    QPixmap* flappymeat;
+    QPixmap* Death;
+    QPixmap* Setting ;
+    QSoundEffect space;
+    QPoint bird_center_;
+    QPoint pos;
+    meatpiece ball;
+    int min_center_y;
+    QLabel *label;
+    QIcon birdicon;
+
+
+    QPushButton *Start_button;
+    QPushButton *Setting_button;
+    QPushButton *Skin_button;
+    QPushButton *Exit_button;
+    QPushButton *Replay_button;
+    QPushButton *Menu_button;
+    QPushButton *Easy_button;
+    QPushButton *Medium_button;
+    QPushButton *Hard_button;
+    QPushButton *ms_button;
+
+    QPushButton *birdorigin_button;
+    QPushButton *birdred_button;
+    QPushButton *hohlobird_button;
+    QPushButton *custom_button;
+    QPushButton *custom_button_picker;
+
+    QPushButton *backgroundorigin_button;
+    QPushButton *custom_background_button;
+    QPushButton *custom_background_button_picker;
+
+};
+#endif // MAINWINDOW_H
